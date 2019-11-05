06b9d4dfef885899e488b727fc4ac8b2b7494571
hill1588 <hill1588@morris.umn.edu>
2019-11-05 11:26:12 -0600

    Create EchoClientRunOut class
    Class reads from the input from the socket and writes to the standard output, while having a constructor that takes a socket.

---
+++ src/echoserver/EchoClientRunOut.java
@@ -0,0 +1,32 @@
+package echoserver;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.lang.Thread;
+
+public class EchoClientRunOut implements Runnable {
+
+    private Socket client;
+
+    public EchoClientRunOut(Socket socket) {
+        client = socket;
+    }
+
+    public void run() {
+        try {
+            InputStream input = client.getInputStream();
+            int read;
+            while ((read = input.read()) != -1) {
+                System.out.write(read);
+            }
+
+            System.out.flush();
+            client.shutdownOutput();
+        } catch (Exception e) {
+            // Do nothing
+        }
+    }
+
+}
